bf52b03d113f103a0a5d880798f0f4476eef62f1
ssotnyk <sotnyk@bk.ru>
2018-01-28 15:40:01 +0200

    Initial code (https://github.com/TelegramBots/telegram.bot.examples as a template)

---
+++ csharp_test_bot/csharp_test_bot/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+//using Telegram.Bot.Types.InputMessageContents;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace csharp_test_bot
+{
+    class Program
+    {
+        const string BotApiKeyName = "csharp_test_bot_key";
+        private static TelegramBotClient Bot;
+        private static User _me;
+
+        private static void Initialize()
+        {
+            string apiKey = Environment.GetEnvironmentVariable(BotApiKeyName);
+            if (string.IsNullOrWhiteSpace(apiKey))
+            {
+                Console.WriteLine($"Could not find environment variable with API token: " + BotApiKeyName);
+                Console.WriteLine("Please, enter it with proper value and run app again.");
+                throw new ArgumentException($"Environment variable '{BotApiKeyName}' not found or empty.");
+            }
+
+            Bot = new TelegramBotClient(apiKey);
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            _me = Bot.GetMeAsync().Result;
+
+            Console.Title = _me.Username;
+        }
+
+        static void Main(string[] args)
+        {
+            Initialize();
+
+            Bot.StartReceiving();
+            Console.WriteLine($"Start listening for @{_me.Username}");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.Text) return;
+
+            //IReplyMarkup keyboard = new ReplyKeyboardRemove();
+
+            switch (message.Text.Split(' ').First())
+            {
+                // send inline keyboard
+                case "/inline":
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                    await Task.Delay(500); // simulate longer running task
+
+                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                    {
+                        new [] // first row
+                        {
+                            InlineKeyboardButton.WithCallbackData("1.1"),
+                            InlineKeyboardButton.WithCallbackData("1.2"),
+                        },
+                        new [] // second row
+                        {
+                            InlineKeyboardButton.WithCallbackData("2.1"),
+                            InlineKeyboardButton.WithCallbackData("2.2"),
+                        }
+                    });
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Choose",
+                        replyMarkup: inlineKeyboard);
+                    break;
+
+                // send custom keyboard
+                case "/keyboard":
+                    ReplyKeyboardMarkup ReplyKeyboard = new[]
+                    {
+                        new[] { "1.1", "1.2" },
+                        new[] { "2.1", "2.2" },
+                    };
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Choose",
+                        replyMarkup: ReplyKeyboard);
+                    break;
+
+                // send a photo
+                case "/photo":
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+
+                    const string file = @"Files/Choco.jpg";
+
+                    var fileName = file.Split(Path.DirectorySeparatorChar).Last();
+
+                    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        await Bot.SendPhotoAsync(
+                            message.Chat.Id,
+                            fileStream,
+                            "Nice Picture");
+                    }
+                    break;
+
+                // request location or contact
+                case "/request":
+                    var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]
+                    {
+                        KeyboardButton.WithRequestLocation("Location"),
+                        KeyboardButton.WithRequestContact("Contact"),
+                    });
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Who or Where are you?",
+                        replyMarkup: RequestReplyKeyboard);
+                    break;
+
+                default:
+                    const string usage = @"Usage:
+/inline   - send inline keyboard
+/keyboard - send custom keyboard
+/photo    - send a photo
+/request  - request location or contact
+";
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        usage,
+                        replyMarkup: new ReplyKeyboardRemove());
+                    break;
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQueryEventArgs.CallbackQuery.Id,
+                $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
+        }
+
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            Console.WriteLine($"Received inline query from: {inlineQueryEventArgs.InlineQuery.From.Id}");
+
+            InlineQueryResultBase[] results = {
+                new InlineQueryResultLocation
+                {
+                    Id = "1",
+                    Latitude = 40.7058316f, // displayed result
+                    Longitude = -74.2581888f,
+                    Title = "New York",
+                    InputMessageContent = new InputLocationMessageContent // message if result is selected
+                    {
+                        Latitude = 40.7058316f,
+                        Longitude = -74.2581888f,
+                    }
+                },
+
+                new InlineQueryResultLocation
+                {
+                    Id = "2",
+                    Longitude = 52.507629f, // displayed result
+                    Latitude = 13.1449577f,
+                    Title = "Berlin",
+                    InputMessageContent = new InputLocationMessageContent // message if result is selected
+                    {
+                        Longitude = 52.507629f,
+                        Latitude = 13.1449577f
+                    }
+                }
+            };
+
+            await Bot.AnswerInlineQueryAsync(
+                inlineQueryEventArgs.InlineQuery.Id,
+                results,
+                isPersonal: true,
+                cacheTime: 0);
+        }
+
+        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine($"Received inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+        }
+    }
+}
+

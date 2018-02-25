832bae5eabf65fc0257d053d435ec9ad2726879d
ssotnyk <sotnyk@bk.ru>
2018-02-25 17:07:05 +0200

    Clear from some unused code

--- csharp_test_bot/csharp_test_bot/Program.cs
+++ csharp_test_bot/csharp_test_bot/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-//using Telegram.Bot.Types.InputMessageContents;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace csharp_test_bot
